5671f6b59d68566dd96d262179d7fd9e805156d2
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-02-15 14:24:37 +0000

    first commit - db created

---
+++ src/main/java/cityservices/com/example/PotholeReport/PotholeReportApplication.java
@@ -0,0 +1,14 @@
+package cityservices.com.example.PotholeReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotholeReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotholeReportApplication.class, args);
+	}
+
+}
+
